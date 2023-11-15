59b3999bbf6d07030f7492060fbb1bd6f3876d14
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2023-11-15 21:33:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+var counterElement = document.getElementById("counter");
+var incrementBtn = document.getElementById("incrementBtn");
+incrementBtn.addEventListener("click", function() {
+            var currentValue = parseInt(counterElement.innerText);
+            alert(currentValue);
+            counterElement.innerText = currentValue + 1;
+});
